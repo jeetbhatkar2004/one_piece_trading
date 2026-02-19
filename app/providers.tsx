de98463d4a35c7c071e98d70aadfa8f00de3ad09
jeetbhatkar2004 <jeetb@gatech.edu>
2026-02-19 15:07:51 -0500

    removed beta screen

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,6 @@
 import { SessionProvider } from 'next-auth/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
-import { BetaGate } from '@/components/BetaGate'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient())
@@ -11,7 +10,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider>
-        <BetaGate>{children}</BetaGate>
+        {children}
       </SessionProvider>
     </QueryClientProvider>
   )
